004fad4e2642bb9196ee825cffaa54765d1f8f9f
Praveen <praveen.kumar.paytm2019@gmail.com>
2023-06-23 18:28:20 +0530

    Second commit : setup express routes for entry point

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const router = require('./routes');
 const app = express();
 const port = 8000;
+
+// use express router
+app.use('/',require('./routes'));
 
 
 app.listen(port, function(err){
