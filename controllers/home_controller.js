64580e37048b79d75bf24be049a67ccc7cc12484
Praveen <praveen.kumar.paytm2019@gmail.com>
2023-06-23 21:16:06 +0530

    create a home.ejs file where we send title value from home_controller.js with the help of res.render('home',{ title:Home});

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,9 @@
 module.exports.home = function(req, res){
-    return res.end('<h1>Express is up for Codeial</h1>');
+    // return res.end('<h1>Express is up for Codeial</h1>');
+
+
+    // This is sending title value to home.ejs file for home page
+    return res.render('home',{
+        title : "Home"
+    });
 }
