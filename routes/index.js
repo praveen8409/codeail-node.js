be2393ecefd17a53ef18ddf6b28c2af28dd2e6e3
Praveen <praveen.kumar.paytm2019@gmail.com>
2023-06-23 19:05:12 +0530

    Create first controller with display some content on browser by access them

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
+// import home_controller to use them
+const homeController = require('../controllers/home_controller');
 
 console.log('Router is loades');
+
+// this is create a url to access homeController in browser
+router.get('/',homeController.home);
 
 module.exports = router;
