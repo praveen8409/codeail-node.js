83109a66e90832b07c40acfdf967c90e083f069c
Praveen <praveen.kumar.paytm2019@gmail.com>
2023-06-23 23:26:08 +0530

    set up static file. Create assets folder inside create three folder and files, then link these files to ejs file

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const port = 8000;
 // This is for use layouts in pages 
 const expressLayouts = require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
 app.use(expressLayouts);
+
+// extract style and script from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 
 // use express router
 app.use('/',require('./routes'));
