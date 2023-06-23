e9b463ecefaaf16158ff9aba2a804bed680d0310
Praveen <praveen.kumar.paytm2019@gmail.com>
2023-06-23 21:33:34 +0530

    Create other ejs file with header and footer

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,8 @@
 module.exports.profile = function(req, res){
-    return res.end('<h1>User Profile </h1>');
+    // return res.end('<h1>User Profile </h1>');
+
+     // This is sending title value to user_profile.ejs file for home page
+     return res.render('user_profile',{
+        title : "Profile"
+    });
 }
