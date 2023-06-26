6288cfe3e49869ea6d6fb77d382dbb02dfc4ddab
Praveen <praveen.kumar.paytm2019@gmail.com>
2023-06-26 17:28:10 +0530

    Display post inside Home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,23 @@
+const Post = require('../models/post');
 module.exports.home = function(req, res){
     // return res.end('<h1>Express is up for Codeial</h1>');
 
     console.log(req.cookies);
     res.cookie('user_id',25); // set the cookie from server side 
     // This is sending title value to home.ejs file for home page
+
+//    Post.find({}, function(err, posts){
+//     return res.render('home',{
+//         title : "Home",
+//         posts : posts
+//     });
+//    });
+
+//populate the user for each post
+   Post.find({}).populate('user').exec(function(err, posts){
     return res.render('home',{
-        title : "Home"
+        title : "Home",
+        posts : posts
     });
+   });
 }
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 
+
 module.exports.profile = function (req, res) {
     // return res.end('<h1>User Profile </h1>');
 
