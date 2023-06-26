f4e2e5a02dbf76ff1be419e6866613bbd9afd420
Praveen <praveen.kumar.paytm2019@gmail.com>
2023-06-26 22:07:48 +0530

    we have done pre-populate and display comments in home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,23 +1,31 @@
 const Post = require('../models/post');
-module.exports.home = function(req, res){
+module.exports.home = function (req, res) {
     // return res.end('<h1>Express is up for Codeial</h1>');
 
     console.log(req.cookies);
-    res.cookie('user_id',25); // set the cookie from server side 
+    res.cookie('user_id', 25); // set the cookie from server side 
     // This is sending title value to home.ejs file for home page
 
-//    Post.find({}, function(err, posts){
-//     return res.render('home',{
-//         title : "Home",
-//         posts : posts
-//     });
-//    });
+    //    Post.find({}, function(err, posts){
+    //     return res.render('home',{
+    //         title : "Home",
+    //         posts : posts
+    //     });
+    //    });
 
-//populate the user for each post
-   Post.find({}).populate('user').exec(function(err, posts){
-    return res.render('home',{
-        title : "Home",
-        posts : posts
-    });
-   });
+    //populate the user for each post
+    Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
+        .exec(function (err, posts) {
+            return res.render('home', {
+                title: "Home",
+                posts: posts
+            });
+        });
 }
