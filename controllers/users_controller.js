a8c1115864a2c751a16cc81b3031ebd06b2e95cb
Praveen <praveen.kumar.paytm2019@gmail.com>
2023-07-05 17:26:17 +0530

    Uploading pic

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -5,32 +5,62 @@ module.exports.profile = function (req, res) {
     // return res.end('<h1>User Profile </h1>');
 
     // This is sending title value to user_profile.ejs file for home page
-    User.findById(req.params.id, function(err, user){
+    User.findById(req.params.id, function (err, user) {
         return res.render('user_profile', {
             title: "Profile",
-            profile_user : user
+            profile_user: user
         });
     });
-   
+
 }
 
 // update user details
-module.exports.update = function(req, res){
-    if(req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
-            req.flash('success', 'Updated!');
+module.exports.update = async function (req, res) {
+    // if(req.user.id == req.params.id){
+    //     User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+    //         req.flash('success', 'Updated!');
+    //         return res.redirect('back');
+    //     });
+    // }else{
+    //     req.flash('error', 'Unauthorized!');
+    //     return res.status(401).send('Unauthorized');
+    // }
+
+
+    if (req.user.id == req.params.id) {
+        try {
+            let user = await User.findByIdAndUpdate(req.params.id);
+            User.uploadedAvatar(req, res, function (err) {
+                if (err) {
+                    console.log("----Multer Erroro-----");
+                    return;
+                }
+
+                user.name = req.body.name;
+                user.email = req.body.email;
+
+                if(req.file){
+                    // this is saving the path of the uploaded file into the avatar field in the user
+                    user.avatar = User.avatarPath + '\\' + req.file.filename;
+                }
+                user.save();
+                return res.redirect('back');
+            });
+        } catch (error) {
+            req.flash('error', error);
             return res.redirect('back');
-        });
-    }else{
+        }
+    } else {
         req.flash('error', 'Unauthorized!');
         return res.status(401).send('Unauthorized');
     }
+
 }
 // render the signUp page
 module.exports.signUp = function (req, res) {
 
     //  if user already signed in but they want to sign up again without logout then they redirect back to profile page
-    if(req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         req.flash('success', 'Signed Up  Successfully');
         return res.redirect('/users/profile');
     }
@@ -46,7 +76,7 @@ module.exports.signUp = function (req, res) {
 module.exports.signIn = function (req, res) {
 
     //  if user already signed in but they want to sign up again without logout then they redirect back to profile page
-    if(req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         req.flash('success', 'Logged in Successfully');
         return res.redirect('/users/profile');
     }
@@ -88,22 +118,21 @@ module.exports.create = function (req, res) {
 
 //  sign in and create session for the user
 module.exports.createSession = function (req, res) {
-    req.flash('success','Logged in successfully');
+    req.flash('success', 'Logged in successfully');
     return res.redirect('/');
 }
 
 // This is for Sign-Out 
-module.exports.destroySession = function(req, res){
-    req.logout(function(err) {
-      if (err) {
-        console.log(err);
-      }
-    req.flash('success','You have logged out');
+module.exports.destroySession = function (req, res) {
+    req.logout(function (err) {
+        if (err) {
+            console.log(err);
+        }
+        req.flash('success', 'You have logged out');
 
-      return res.redirect('/users/sign-up');
+        return res.redirect('/users/sign-up');
     });
-  }
+}
 
 
-  
-  
+
