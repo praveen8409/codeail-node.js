7035ce8b1110255bfc8165da542deb74e63ae371
Praveen <praveen.kumar.paytm2019@gmail.com>
2023-07-11 12:39:50 +0530

    Set mail template using ejs;

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -5,11 +5,13 @@ const nodeMailer = require('../config/nodemailer');
 exports.newComment = (comment) => {
     console.log('inside newComment mailer', comment);
 
+    let htmlString = nodeMailer.renderTemplate({comment : comment}, '/comments/new_comment.ejs');
+
     nodeMailer.transporter.sendMail({
        from: 'praveen.kumar.nord2@gmail.com',
        to: comment.user.email,
        subject: "New Comment Published!",
-       html: '<h1>Yup, your comment is now published!</h1>' 
+       html: htmlString
     }, (err, info) => {
         if (err){
             console.log('Error in sending mail', err);
