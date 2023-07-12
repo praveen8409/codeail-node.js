22ffc76192108c986dc1f98971c58f601d9018b6
Praveen <praveen.kumar.paytm2019@gmail.com>
2023-07-12 11:09:04 +0530

    Create schema for likes

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,6 +1,8 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 const commentsMailer = require('../mailers/comments_mailer');
+const queue = require('../config/kue');
+const commentEmailWorker = require('../workers/comment_email_worker');
 
 module.exports.create = async function(req, res){
 
@@ -18,7 +20,14 @@ module.exports.create = async function(req, res){
             post.save();
             
             comment = await comment.populate('user', 'name email');
-            commentsMailer.newComment(comment);
+            // commentsMailer.newComment(comment);
+           let job = queue.create('emails', comment).save(function(err){
+                if(err){
+                    console.log('error in sending to the queue',err);
+                    return;
+                }
+                console.log('job enqued',job.id);
+            });
             if (req.xhr){
                 
     
