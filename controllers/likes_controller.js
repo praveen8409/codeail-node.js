8d190f9c57cc5e4ce7366675d08cd214f4897911
Praveen <praveen.kumar.paytm2019@gmail.com>
2023-07-13 21:38:44 +0530

    Setup Like in post and comment

--- controllers/likes_controller.js
+++ controllers/likes_controller.js
@@ -1,57 +1,65 @@
-const Like = require('../models/like');
-const Post = require('../models/post');
+const Like = require("../models/like");
+const Post =  require("../models/post");
 const Comment = require('../models/comment');
 
-module.exports.toogleLike = async function(req, res){
-    try {
-        
-        // likes/toggle/?id=abcd=Post
+
+module.exports.toggleLike = async function(req, res){
+    try{
+
+        // likes/toggle/?id=abcdef&type=Post
         let likeable;
         let deleted = false;
 
-        if(req.query.type == 'Post'){
+
+        if (req.query.type == 'Post'){
             likeable = await Post.findById(req.query.id).populate('likes');
         }else{
-            likeable = await Comment.findById(req.query.id).populate('like');
+            likeable = await Comment.findById(req.query.id).populate('likes');
         }
+
 
         // check if a like already exists
-
         let existingLike = await Like.findOne({
-            likeable : req.query.id,
-            onModel : req.query.type,
-            user : req.user._id
-        });
+            likeable: req.query.id,
+            onModel: req.query.type,
+            user: req.user._id
+        })
 
-        //if a like already exists then delete it
-        if(existingLike){
-            likeable.likes.pull(existingLike.like._id);
-            like.save();
+        // if a like already exists then delete it
+        if (existingLike){
+            likeable.likes.pull(existingLike._id);
+            likeable.save();
 
             existingLike.remove();
-            deleted : true;
+            deleted = true;
+
         }else{
             // else make a new like
+
             let newLike = await Like.create({
-                user : req.user._id,
-                likeable : req.query.id,
-                onModel : req.query.type
+                user: req.user._id,
+                likeable: req.query.id,
+                onModel: req.query.type
             });
 
-            likeable.likes.push(like._id);
+            likeable.likes.push(newLike._id);
             likeable.save();
+
         }
 
-        return res.join(200, {
-            message : "Remove successfull",
-            data : {
-                deleted : deleted
+        return res.json(200, {
+            message: "Request successful!",
+            data: {
+                deleted: deleted
             }
         })
-    } catch (error) {
+
+
+
+    }catch(err){
         console.log(err);
-        return res.join(500, {
-            message : 'Internal server error'
+        return res.json(500, {
+            message: 'Internal Server Error'
         });
     }
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,7 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
+const Like = require('../models/like');
+
 
 
 // module.exports.create = function(req, res){
@@ -73,6 +75,12 @@ module.exports.destroy = async function(req, res){
         let post = await Post.findById(req.params.id);
 
         if (post.user == req.user.id){
+
+              // CHANGE :: delete the associated likes for the post and all its comments' likes too
+              await Like.deleteMany({likeable: post, onModel: 'Post'});
+              await Like.deleteMany({_id: {$in: post.comments}});
+
+              
             post.remove();
 
             await Comment.deleteMany({post: req.params.id});
