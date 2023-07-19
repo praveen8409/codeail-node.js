f7e3cf7313fd777e49c8f79633aa9ef3559961e7
Praveen <praveen.kumar.paytm2019@gmail.com>
2023-07-19 18:18:12 +0530

    create environment

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,16 +1,10 @@
 const nodemailer = require("nodemailer");
 const ejs = require('ejs');
-const path = require('path')
+const path = require('path');
+const env = require('./environment');
 
 
-const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'trey.prosacco@ethereal.email',
-        pass: 'q9gxQzn3rqKpPDZWNW'
-    }
-});
+let transporter = nodemailer.createTransport(env.smtp);
 
 
 let renderTemplate = (data, relativePath) => {
