cbecdf97bb014c0a56255ad0d2cc105068459cf9
Praveen <praveen.kumar.paytm2019@gmail.com>
2023-07-26 11:53:06 +0530

    joined chatroom

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -1,20 +1,27 @@
-class ChatEngine{
-    constructor(chatBoxId, userEmail){
-        this.chatBox = $(`#${chatBoxId}`);
-        this.userEmail = userEmail;
-
-        this.socket = io.connect('http://localhost:5000');
-
-        if (this.userEmail){
-            this.connectionHandler();
-        }
-
+class ChatEngine {
+    constructor(chatBoxId, userEmail) {
+      this.chatBox = $(`#${chatBoxId}`);
+      this.userEmail = userEmail;
+      this.socket = io.connect('http://localhost:5000');
+  
+      if (this.userEmail) {
+        this.connectionHandler();
+      }
     }
-
-
-    connectionHandler(){
-        this.socket.on('connect', function(){
-            console.log('connection established using sockets...!');
+  
+    connectionHandler() {
+      this.socket.on('connect', () => { // Use arrow function here
+        console.log('connection established using sockets...!');
+        
+        this.socket.emit('join_room', {
+          user_email: this.userEmail,
+          chatroom: 'codeial',
         });
+  
+        this.socket.on('user_joined', (data) => { // Use arrow function here
+          console.log('a user joined!', data);
+        });
+      });
     }
-}
+  }
+  
